4cf4f52a72d34e9eac78c987d76d2c1c21323d98
Chakorn2 <chakorn7658@gmail.com>
2024-01-09 14:22:05 +0700

    Update myroute.blade.php

--- example-app/example-app/example-app/resources/views/myroute.blade.php
+++ example-app/example-app/example-app/resources/views/myroute.blade.php
@@ -6,5 +6,11 @@
     <body>
         <h1>My Route Page</h1>
         <h1>{{ $myinput }}</h1>
+    <h1>ตารางสูตรคูณแม่ <?php echo $myinput ?></h1>
+    <div>
+        <?php for($i = 1; $i <= 24; $i++) {?>
+            <?php echo $myinput ?> x <?php echo $i ?> = <?php echo $myinput*$i ?><br>
+        <?php } ?>
+    </div>
     </body>
 </html>
