7c499852ba852141458ce11784c1e0511f3a6a06
Chakorn2 <chakorn7658@gmail.com>
2024-01-09 14:21:59 +0700

    Update mypage.blade.php

--- example-app/example-app/example-app/resources/views/myfolder/mypage.blade.php
+++ example-app/example-app/example-app/resources/views/myfolder/mypage.blade.php
@@ -5,6 +5,6 @@
 <h1>{{$val_a . ' ' .$val_b}}</h1>
 <form method="post" action="/my-route">
     @csrf
-    <input type="text" name="myinput">
+    <input type="number" name="myinput">
     <button type="submit">Submit</button>
 </form>
