13c6e6e6642fc847ab887882904f122f50a245ab
Chakorn2 <chakorn7658@gmail.com>
2024-01-09 19:57:33 +0700

    Update myroute.blade.php

--- example-app/example-app/example-app/resources/views/myroute.blade.php
+++ example-app/example-app/example-app/resources/views/myroute.blade.php
@@ -1,16 +1,33 @@
 <html>
     <head>
-        <meta charset="utf-8"/>
+        <meta charset= "utf-8">
     </head>
-
-    <body>
-        <h1>My Route Page</h1>
-        <h1>{{ $myinput }}</h1>
-    <h1>ตารางสูตรคูณแม่ <?php echo $myinput ?></h1>
-    <div>
-        <?php for($i = 1; $i <= 24; $i++) {?>
-            <?php echo $myinput ?> x <?php echo $i ?> = <?php echo $myinput*$i ?><br>
-        <?php } ?>
-    </div>
+    <body bgcolor="#2F4F4F">
+        <center>
+            <h1>ตารางสูตรคูณแม่ {{ $myinput }}</h1>
+            <div class="box">
+                <div>
+                    <b>
+                        <?php
+                        for ($i = 1; $i <= 24; $i++) {
+                            echo "{$myinput} x {$i} = " . $myinput * $i . '<br/>';
+                        }
+                        ?>
+                    </b>
+                </div>
+            </div>
+        </center>
     </body>
+    <style>
+        .box{
+            background:transparent	;
+            width: 250px;
+            height: 500px;
+            color: #ffffff;
+        }
+        h1{
+            color: #ffffff;
+            size:200px;
+        }
+    </style>
 </html>
