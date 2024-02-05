0ce0e700b234fc8f1d10ade7c3e3259a92cc0101
Chakorn2 <chakorn7658@gmail.com>
2024-02-05 12:32:59 +0700

    Update web.php

--- example-app/example-app/example-app/routes/web.php
+++ example-app/example-app/example-app/routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\C_titles;
+use App\Http\Controllers\MyAuth;
 use App\Http\Controllers\MyController;
 use Illuminate\Support\Facades\Route;
 use Illuminate\http\Request;
@@ -13,6 +15,18 @@ use Illuminate\http\Request;
 | contains the "web" middleware group. Now create something great!
 |
 */
+
+Route::get('/login', [MyAuth::class, 'login_view'])->name('login');
+Route::get('/register', [MyAuth::class, 'register_view']);
+Route::get('/logout', [MyAuth::class, 'logout_process']);
+Route::post('/login', [MyAuth::class, 'login_process']);
+Route::post('/register', [MyAuth::class, 'register_process']);
+
+Route::resource('titles', C_titles::class)->middleware('auth');
+Route::middleware('auth')->group(function(){
+    // auth first
+});
+
 Route::get('/my-controller',[MyController::class,'index'] );
 Route::get('/my-controller2','App\Http\Controllers\MyController@index');
 Route::resource('/my-controller3', MyController::class);
